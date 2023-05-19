dc49a23bf68456c70a7a70ee8e690b311beab1ab
pinof <fattoruso.g@gmail.com>
2023-05-19 15:12:03 +0200

    refactoring

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -23,15 +23,16 @@ class TodoListTest extends TestCase
         $this->list = TodoList::factory()->create();
     }
 
-/*
+
     public function test_fetch_all_todo_list(): void
     {
         #disabilita la gestione delle eccezioni
         $this->withoutExceptionHandling();
 
         # preparation / prepare
-        # TodoList::create(["name" => "my list"]);
-        $list = TodoList::factory()->create();
+        ##TodoList::create(["name" => "my list"]);
+        ##$list = TodoList::factory()->create();
+        ## PS:. the factory is generate in setUp function
 
         # action / perform
         $response = $this->getJson(route('todo-list.index'));
@@ -44,25 +45,26 @@ class TodoListTest extends TestCase
         $response->assertStatus(200);
 
     }
-*/
-/*
+
+
     public function test_fetch_single_todo_list(){
 
         # disabilita la gestione delle eccezioni
         $this->withoutExceptionHandling();
 
         #preparation
-        $list = TodoList::factory()->create();
+        ## $list = TodoList::factory()->create();
+        ## PS:. the factory is generate in setUp function
 
         #action
-        $response = $this->getJson(route('todo-list.show',$list->id))
+        $response = $this->getJson(route('todo-list.show' , $this->list->id))
             ->assertOk()
             ->json();
 
 
         #assertion
-        $this->assertEquals($response["name"], $list->name);
-    }*/
+        $this->assertEquals($response["name"], $this->list->name);
+    }
 
 
     public function test_store_new_todo_list(){
@@ -71,17 +73,18 @@ class TodoListTest extends TestCase
         $this->withoutExceptionHandling();
 
         # preparation
-        $list = TodoList::factory()->create();
+        #$list = TodoList::factory()->create();
+        ## PS:. the factory is generate in setUp function
 
         # action
-        $response = $this->postJson(route('todo-list.store', ["name" => $list->name]))
+        $response = $this->postJson(route('todo-list.store', ["name" => $this->list->name]))
             ->assertCreated()
             ->json();
 
-        $this->assertEquals($list->name, $response["name"]);
+        $this->assertEquals($this->list->name , $response["name"]);
 
         # assertion
-        $this->assertDatabaseHas('todo_lists', ["name" => $list->name]);
+        $this->assertDatabaseHas('todo_lists', ["name" => $this->list->name]);
 
     }
 
@@ -109,14 +112,15 @@ class TodoListTest extends TestCase
         $this->withoutExceptionHandling();
 
         #preparation
-        $list = TodoList::factory()->create();
+        # $list = TodoList::factory()->create();
+        ## PS:. the factory is generate in setUp function
 
         #action
-        $response = $this->deleteJson(route('todo-list.destroy', $list->id))
+        $response = $this->deleteJson(route('todo-list.destroy', $this->list->id))
             ->assertNoContent();
 
         #assertion
-        $this->assertDatabaseMissing('todo_lists', ['name' => $list->name]);
+        $this->assertDatabaseMissing('todo_lists', ['name' => $this->list->name]);
 
     }
 
